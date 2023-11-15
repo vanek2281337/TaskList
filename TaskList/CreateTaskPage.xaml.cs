9b93d4d15a447b498ac1f5740ab34c681e9f7ca3
STUDENT-002 <STUDENT-002@LVB-STUDENT-002>
2023-11-15 13:02:26 +0700

    33

--- TaskList/CreateTaskPage.xaml.cs
+++ TaskList/CreateTaskPage.xaml.cs
@@ -7,4 +7,20 @@ public partial class CreateTaskPage : ContentPage
         InitializeComponent();
 
     }
+     public async void Send__Click(object s, EventArgs e)
+    {
+        if (Title.Text == "" || Description.Text == "")
+        {
+            await DisplayAlert( "error",  "error2",  "error3" );
+           
+        }
+
+        if (Title.Text.Length > 0 && Description.Text.Length > 0)
+        {
+            
+            await Navigation.PushAsync(new MainPage());
+            
+        }
+    }
+
 }
